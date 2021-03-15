752d899674e8d938b3dfbfe037dc0c3debeea2b2
Alex <poprygun@hotmail.com>
2021-03-15 13:48:28 -0400

    Cleanup.

--- oauth2-server/src/main/java/io/microsamples/security/oauth2server/UserController.java
+++ oauth2-server/src/main/java/io/microsamples/security/oauth2server/UserController.java
@@ -1,2 +1,16 @@
-package io.microsamples.security.oauth2server;public class UserController {
+package io.microsamples.security.oauth2server;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class UserController {
+    @GetMapping("/user")
+    Map<String, Object> user(Authentication authentication) {
+        return Collections.singletonMap("sub", authentication.getName());
+    }
 }
